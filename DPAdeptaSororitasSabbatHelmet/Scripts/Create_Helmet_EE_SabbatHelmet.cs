b2f6ada6b003dbdf1f8eae34b2c1de02b41290d4
DarthParametric <46890653+DarthParametric@users.noreply.github.com>
2024-12-01 17:28:10 +1000

    Git doesn't like junctions. Whoops.

---
+++ DPAdeptaSororitasSabbatHelmet/Scripts/Create_Helmet_EE_SabbatHelmet.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Kingmaker.Visual.CharacterSystem;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Equipment Entities/Helmets/EE_AdeptaSororitasSabbatHelmetDP_F_HM")]
+public class EE_AdeptaSororitasSabbatHelmetDP_F_HM : EquipmentEntity
+{
+
+}
---
+++ DPAdeptaSororitasSabbatHelmet/Scripts/DPAdeptaSororitasSabbatHelmet_CreateNewModEE.cs
@@ -0,0 +1,10 @@
+using Kingmaker.Visual.CharacterSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Equipment Entities/New EE")]
+public class DPAdeptaSororitasSabbatHelmet_CreateNewModEE : EquipmentEntity
+{
+
+}
